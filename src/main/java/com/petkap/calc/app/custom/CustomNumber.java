8b3a81f1176a1976e8a5a346f850ea067f695d62
PetKap <pet.kapralcik@gmail.com>
2024-08-22 14:51:02 +0200

    Implement CustomNumber calculation

--- src/main/java/com/petkap/calc/app/custom/CustomNumber.java
+++ src/main/java/com/petkap/calc/app/custom/CustomNumber.java
@@ -2,33 +2,120 @@ package com.petkap.calc.app.custom;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import com.petkap.calc.app.helper.StringHelper;
+import com.petkap.calc.app.util.StringUtil;
 
 public class CustomNumber {
 	private String number;
+	private StringUtil su = new StringUtil();
+
+	public CustomNumber() {
+	}
 
 	public CustomNumber(String n) {
 		number = n;
 	}
 
-	public CustomNumber multiply(CustomNumber n1) {
-		var sh = new StringHelper();
-		var cn1 = sh.reverseArray(number.toCharArray());
-		var cn2 = sh.reverseArray(n1.getNumber().toCharArray());
-		var res = new StringBuilder();
-		List<StringBuilder> partialRes = new ArrayList<>();
+	public CustomNumber multiply(CustomNumber n2) {
+		// fix case when it's negative number
+		var needToHandleN1 = number.startsWith("-");
+		var needToHandleN2 = n2.getNumber().startsWith("-");
+		var negResult = (needToHandleN1 && !needToHandleN2) || (!needToHandleN1 && needToHandleN2);
+
+		var handledN1 = needToHandleN1 ? number.substring(1) : number;
+		var handledN2 = needToHandleN2 ? n2.getNumber().substring(1) : n2.getNumber();
+
+		var cn1 = su.reverseArray(handledN1.toCharArray());
+		var cn2 = su.reverseArray(handledN2.toCharArray());
+
+		var partialRes = calculatePartialResults(cn1, cn2);
+		var result = calculateResult(su.makeRowsSameSize(partialRes));
+
+		return new CustomNumber(String.format("%s%s", negResult ? "-" : "", result));
+	}
+
+	/**
+	 * Calculates partial results. Example: 123 * 123 will crate list which looks
+	 * like this:<br>
+	 * 123<br>
+	 * 0246<br>
+	 * 00369<br>
+	 * Number cn1 is multiplied by every digit from cn2. Partial result means cn1 is
+	 * multiplied by digit from cn2, where offset (num of zeroes at the beginning)
+	 * is calculated based on position of digit from cn2.
+	 *
+	 * @param cn1 first number used for multiplication
+	 * @param cn2 second number used for multiplication
+	 * @return list of numbers
+	 */
+	protected List<String> calculatePartialResults(char[] cn1, char[] cn2) {
+		List<String> res = new ArrayList<>();
+
+		for (int i = 0; i < cn1.length; i++) {
+			char num = cn1[i];
+			var str = new StringBuilder();
+			int onHold = 0;
+
+			str.append(su.createZeroesString(i));
+
+			for (int j = 0; j < cn2.length; j++) {
+				int currValue = multiplySimpleDigits(cn2[j], num);
+				str.append((currValue + onHold) % 10);
+				onHold = (currValue + onHold) / 10;
+			}
+
+			if (onHold > 0) {
+				str.append(onHold);
+			}
+
+			res.add(str.toString());
+		}
+
+		return res;
+	}
+
+	/**
+	 * Multiplies number c1 and c2.
+	 *
+	 * @param c1 first number to use
+	 * @param c2 second number to use
+	 * @return multiplied value
+	 */
+	protected int multiplySimpleDigits(char c1, char c2) {
+		var n1 = Character.getNumericValue(c1);
+		var n2 = Character.getNumericValue(c2);
+
+		return n1 * n2;
+	}
+
+	/**
+	 * Goes through list of values and does addition for columns.
+	 *
+	 * @param partialRes list of partial results
+	 * @return calculated value
+	 */
+	protected String calculateResult(List<String> partialRes) {
+		char[][] pr = su.createArray(partialRes.stream().map(String::toCharArray).collect(Collectors.toList()));
 		int onHold = 0;
+		var res = new StringBuilder();
 
-		// 12345 * 12345
-		// -----
-		// 61725 offset 0
-		// + 49380 offset 1
-		// + 37035 offset 2
-		// + 24690 offset 3
-		// + 12345 offset 5
+		for (int clm = 0; clm < pr[0].length; clm++) {
+			int currClm = onHold;
 
-		return new CustomNumber(res.toString());
+			for (int row = 0; row < pr.length; row++) {
+				currClm += Character.getNumericValue(pr[row][clm]);
+			}
+
+			res.append(currClm % 10);
+			onHold = currClm / 10;
+		}
+
+		if (onHold > 0) {
+			res.append(onHold);
+		}
+
+		return res.reverse().toString();
 	}
 
 	public String getNumber() {
--- src/test/java/com/petkap/calc/app/custom/CustomNumberTest.java
+++ src/test/java/com/petkap/calc/app/custom/CustomNumberTest.java
@@ -1,7 +1,10 @@
 package com.petkap.calc.app.custom;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,38 +12,57 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class CustomNumberTest {
-	private static Stream<Arguments> inputsAndResults() {
+	private CustomNumber cn = new CustomNumber();
+
+	private static Stream<Arguments> IandRCalcPartRes() {
 		return Stream.of(//
-				Arguments.of("0", "0", "0"), //
-				Arguments.of("1000", "1000", "1000000"), //
-				Arguments.of("100000", "100000", "10000000000"), //
-				Arguments.of("-2147483648", "-2147483648", "4611686018427387904"), //
-				Arguments.of("2147483648", "2147483648", "4611686018427387904"), //
-				Arguments.of("-2147483648", "2147483648", "-4611686018427387904"), //
-				Arguments.of("1234567890", "9876543210", "12193263111263526900"), //
-				Arguments.of("-1234567890", "-9876543210", "12193263111263526900"), //
-				Arguments.of("-1234567890", "9876543210", "-12193263111263526900"), //
-				Arguments.of("12345678901234567890", "98765432109876543210",
-						"1219326311370217952237463801111263526900"), //
-				Arguments.of("-12345678901234567890", "-98765432109876543210",
-						"1219326311370217952237463801111263526900"), //
-				Arguments.of("-12345678901234567890", "98765432109876543210",
-						"-1219326311370217952237463801111263526900"), //
-				Arguments.of("123456789012345678901234567890", "123456789012345678901234567890",
-						"15241578753238836750495351562536198787501905199875019052100"), //
-				Arguments.of("-123456789012345678901234567890", "-123456789012345678901234567890",
-						"15241578753238836750495351562536198787501905199875019052100"), //
-				Arguments.of("-123456789012345678901234567890", "123456789012345678901234567890",
-						"-15241578753238836750495351562536198787501905199875019052100")//
+				Arguments.of(new char[] { '1', '2', '3' }, new char[] { '1', '2', '3' },
+						Arrays.asList("123", "0246", "00369")), //
+				Arguments.of(new char[] { '1' }, new char[] { '1' }, Arrays.asList("1")), //
+				Arguments.of(new char[] { '5', '0' }, new char[] { '5', '0' }, Arrays.asList("52", "000")) //
+		);
+	}
+
+	private static Stream<Arguments> IandRMultiplySD() {
+		return Stream.of(//
+				Arguments.of('0', '0', 0), //
+				Arguments.of('1', '1', 1), //
+				Arguments.of('5', '5', 25), //
+				Arguments.of('2', '7', 14), //
+				Arguments.of('2', '7', 14), //
+				Arguments.of('2', '7', 14), //
+				Arguments.of('9', '9', 81) //
+		);
+	}
+
+	private static Stream<Arguments> IandRCalcRes() {
+		return Stream.of(//
+				Arguments.of(Arrays.asList("0012", "0023"), "5300"), //
+				Arguments.of(Arrays.asList("001200", "002300"), "005300"), //
+				Arguments.of(Arrays.asList("123", "321"), "444"), //
+				Arguments.of(Arrays.asList("01230", "03210"), "04440"), //
+				Arguments.of(Arrays.asList("1230", "3210"), "0444"), //
+				Arguments.of(Arrays.asList("0123", "3210"), "3333"), //
+				Arguments.of(Arrays.asList("0"), "0") //
 		);
 	}
 
 	@ParameterizedTest
-	@MethodSource("inputsAndResults")
-	void testSimpleMultiplication(String num1, String num2, String result) {
-		CustomNumberCalculator cnc = new CustomNumberCalculator(num1, num2);
-		CustomNumber res = new CustomNumber(result);
-
-		assertEquals(res, cnc.getResult(), String.format("Wrong calculation for: %s * %s.", num1, num2));
+	@MethodSource("IandRCalcPartRes")
+	void testCalculatePartialResults(char[] n1, char[] n2, List<String> res) {
+		assertLinesMatch(res, cn.calculatePartialResults(n1, n2));
 	}
+
+	@ParameterizedTest
+	@MethodSource("IandRMultiplySD")
+	void testMultiplySimpleDigits(char n1, char n2, int res) {
+		assertEquals(res, cn.multiplySimpleDigits(n1, n2));
+	}
+
+	@ParameterizedTest
+	@MethodSource("IandRCalcRes")
+	void testCalculateResult(List<String> list, String res) {
+		assertEquals(res, cn.calculateResult(list));
+	}
+
 }
